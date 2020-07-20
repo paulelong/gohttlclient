0b546219d8ead4946797553c3c87bc0e01cf0754
Paul Long <plong@axon.com>
2020-07-20 15:17:26 -0400

    First commit

---
+++ client.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	url := "http://localhost:8090/v1/teachers"
+
+
+	d := bytes.NewBuffer((make([]byte, 1000000, 1000000)))
+
+	req, err := http.NewRequest("POST", url, d)
+	// req, err := http.NewRequest("POST", url, b)
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/octet-stream")
+	// req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Host", "httpbin.org")
+
+	// Create and Add cookie to request
+	cookie := http.Cookie{Name: "cookie_name", Value: "cookie_value"}
+	req.AddCookie(&cookie)
+
+	// Set client timeout
+	client := &http.Client{Timeout: time.Second * 10}
+
+	// Validate cookie and headers are attached
+	fmt.Println(req.Cookies())
+	fmt.Println(req.Header)
+
+	// Send request
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+
+	fmt.Printf("%s\n", body)
+}
